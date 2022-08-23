f71939e08cd62f3723ad805ef7b3263a6f670601
Valeria Glazunova <valeriaglazunova@gmail.com>
2022-08-23 16:54:06 +0500

    переиспользовала шаблон секции, добавила инпуты стилей и заголовка секции

--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-section',
   templateUrl: './section.component.html',
   styleUrls: ['./section.component.css']
 })
-export class SectionComponent {};
+export class SectionComponent {
+  @Input()title: string = 'Idea';
+  taskNumber: number = 1;
+  @Input()color: string = 'rgb(255, 0, 0)';
+};
