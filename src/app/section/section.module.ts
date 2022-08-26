efbfa40c2545c1029949ab4526d9f998a15fef61
Valeria Glazunova <valeriaglazunova@gmail.com>
2022-08-26 11:44:48 +0500

    Добавила компонент и верстку модального окна

--- src/app/section/section.module.ts
+++ src/app/section/section.module.ts
@@ -7,6 +7,7 @@ import localeRu from '@angular/common/locales/ru';
 import { HeaderComponent } from '../header/header.component';
 import { SectionComponent } from '../section/section.component';
 import { TaskComponent } from '../task/task.component';
+import { ModalformComponent } from '../modalform/modalform.component';
 
 registerLocaleData(localeRu, 'ru')
 
@@ -14,12 +15,14 @@ registerLocaleData(localeRu, 'ru')
   declarations: [
     HeaderComponent,
     SectionComponent,
-    TaskComponent
+    TaskComponent,
+    ModalformComponent
   ],
   exports: [
     HeaderComponent,
     SectionComponent,
     TaskComponent,
+    ModalformComponent
   ],
   imports: [
     BrowserModule,
